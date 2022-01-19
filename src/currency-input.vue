256405b6807a69d49b24fe2d2d2e39ff6a048477
Marty <martin.d.weel@icloud.com>
2022-01-19 15:58:37 -0800

    bug fix for updating in the middle of a value after removing the functionality for safari

--- src/currency-input.vue
+++ src/currency-input.vue
@@ -99,11 +99,14 @@ export default {
       input_val = this.doFormat(input_val, type);
       this.innerValue = input_val;
 
-      // this.$nextTick(() => {
-      //   var updated_len = input_val.length;
-      //   caret_pos = updated_len - original_len + caret_pos;
-      //   input.srcElement.setSelectionRange(caret_pos, caret_pos);
-      // });
+      this.$nextTick(() => {
+        if (type != "blur") {
+          //safari didnt like this
+          var updated_len = input_val.length;
+          caret_pos = updated_len - original_len + caret_pos;
+          input.srcElement.setSelectionRange(caret_pos, caret_pos);
+        }
+      });
     },
   },
 };
